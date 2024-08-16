c7cdd74a88c26fc51225e0731c57a9afe2814e91
hosseinhashemi <hashemihossein7899@gmail.com>
2024-08-17 00:12:54 +0330

    generating user-service sub-project

--- apps/book-service/test/app.e2e-spec.ts
+++ apps/book-service/test/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { BookServiceModule } from './../src/book-service.module';
 
-describe('AppController (e2e)', () => {
+describe('BookServiceController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [BookServiceModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
