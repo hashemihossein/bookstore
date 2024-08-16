0038ba7df36ddc3441c237555911c4e2544aa8c6
hosseinhashemi <hossein.hashhemi@gmail.com>
2024-08-17 03:16:10 +0330

    adding create book DTO

--- apps/book-service/books/dto/create-book.dto.ts
+++ apps/book-service/books/dto/create-book.dto.ts
@@ -1,1 +1,33 @@
-export class CreateBookDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  IsOptional,
+  IsBoolean,
+} from 'class-validator';
+
+export class CreateBookDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  author: string;
+
+  @IsOptional()
+  @IsString()
+  genre?: string;
+
+  @IsOptional()
+  @IsString()
+  releaseDate?: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+
+  @IsOptional()
+  @IsBoolean()
+  isPremium: boolean;
+}
