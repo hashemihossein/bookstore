44bdb72b16e3d5296955f3715383ed60dc883465
hosseinhashemi <hashemihossein7899@gmail.com>
2024-08-17 03:11:46 +0330

    adding premium property to book schema

--- libs/db/src/schemas/book.schema.ts
+++ libs/db/src/schemas/book.schema.ts
@@ -17,6 +17,9 @@ export class Book extends Document {
 
   @Prop({ required: true, min: 0 })
   price: number;
+
+  @Prop({ required: true, default: false })
+  isPremium: boolean;
 }
 
 export const BookSchema = SchemaFactory.createForClass(Book);
